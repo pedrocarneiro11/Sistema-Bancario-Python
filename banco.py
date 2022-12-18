6ef887208a670c3641fd59b697c4837092ccaf26
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2022-12-18 10:26:35 -0300

    Terminando os metodos de deposito e saque

--- banco.py
+++ banco.py
@@ -7,13 +7,14 @@ menu = """
 => """
 
 saldo = float(0)
-limite = 500
+limite = float(500)
 extrato = ""
 numero_saques = 0
+numero_depositos = 0
 LIMITE_SAQUES = 3
 
 def depositar():
-    global saldo
+    global saldo, numero_depositos
 
     valor = input("Digite o valor a ser depositado: ")
     deposito = (float(valor))
@@ -22,27 +23,45 @@ def depositar():
         deposito = input("Digite um valor valido para deposito: ")
 
     saldo = saldo + deposito
+    numero_depositos = numero_depositos + 1
 
-    return saldo
+    return saldo, numero_depositos
 
+def sacar():
+    global saldo, numero_saques
+    if (numero_saques < LIMITE_SAQUES) :
+        valor = float(input("Digite o valor para sacar: "))
+        while valor > limite:
+            print("Valor digitado superior ao seu limite de " + str(limite))
+            valor = float(input("Digite um valor inferior ao seu limite para sacar: "))
+        while valor > saldo:
+            valor = float(input("Digite um valor inferior ao seu saldo: "))
+        
+        saldo = saldo - valor
+        numero_saques = numero_saques + 1
 
-
-
+        return saldo, numero_saques  
+    
+    else :
+        print("Limite diário de saques atingido")
 
 
 while True:
     opcao = input(menu)
 
-    if opcao == "1":
+    if opcao == "1": 
         print("Deposito")
         depositar()
         print("Saldo restante: ", saldo)
     
     elif opcao == "2":
         print("Saque")
+        sacar()
+        print("Saldo restante: ", saldo)
     
     elif opcao == "3":
         print("Extrato")
+        print("Seu saldo: ", saldo)
     
     elif opcao == "4":
         print("Saindo...")
