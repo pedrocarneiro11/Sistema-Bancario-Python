ffff858cdf8661457908fba02fca5cafe8c140c0
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2022-12-29 11:26:30 -0300

    mapeamento do metodo de deposito

--- banco_POO.py
+++ banco_POO.py
@@ -169,3 +169,17 @@ class Saque(Transacao):
 
         if sucesso_transacao:
             conta.historico.adicionar_transacao(self)
+
+class Deposito(Transacao):
+    def __init__(self, valor):
+        self.valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+
+    def registrar(self, conta):
+        sucesso_transacao = conta.depositar(self.valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
